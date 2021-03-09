d72bac9ea4a1d2237dba26ae3d4960448c82565b
BogdanCikota <bcikota@live.com>
2021-03-09 12:31:36 +0100

    Make useFetch hook

--- src/App.js
+++ src/App.js
@@ -1,52 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import running from './images/running.png';
 import aerobics from './images/aerobics.png';
+import useFetch from './useFetch';
 
 function App() {
   const [url, setUrl] = useState('feed/here/?');
-  const [location, setLocation] = useState('');
-  const [aqi, setAqi] = useState(null);
   const [searchValue, setSearchValue] = useState('');
-
-
-  useEffect(() => {
-    fetch(`https://api.waqi.info/${url}token=c0a9c351f889b4756f46957e26d8f31376df7e1f`)
-      .then(res => {
-        return res.json();
-      })
-      .then(obj => {
-
-        if (url === 'feed/here/?') {
-          setLocation(obj.data.city.name);
-          setAqi(obj.data.aqi);
-        } else {
-          setLocation(obj.data[0].station.name);
-          setAqi(obj.data[0].aqi);
-        }
-
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, [url]);
-
-  useEffect(() => {
-    (aqi > 300) ?
-      document.body.style = `background: brown` :
-      (aqi > 200) ?
-        document.body.style = `background: purple` :
-        (aqi > 150) ?
-          document.body.style = `background: red` :
-          (aqi > 100) ?
-            document.body.style = `background: orange` :
-            (aqi > 50) ?
-              document.body.style = `background: yellow` :
-              (aqi > 0) ?
-                document.body.style = `background: green` :
-                document.body.style = `background: lightgray`
-  }, [aqi]);
+  const {location, aqi} = useFetch(url);
+  
 
   return (
     <div className="App">
